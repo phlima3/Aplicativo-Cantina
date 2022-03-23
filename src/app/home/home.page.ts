ba9b141d3405ae0145277e11a58a2334f45be2c1
Pedro Lima <psantoslima86@gmail.com>
2022-03-23 20:21:08 -0300

    Pedro
    Adicionada função de exibir troco

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,64 +11,64 @@ export class HomePage {
   qtdBala: number = 0;
   qtdChocolate: number = 0;
   resultado: number = 0;
+  valorRecebido: number = 0;
+  resultadoTroco: number = 0;
 
   constructor() {}
-calcularTotal(){
-    this.resultado = this.qtdHotdog *5;
-    this.resultado += this.qtdRefri *3.5;
-    this.resultado += this.qtdBala *0.25;
-    this.resultado += this.qtdChocolate *2;
+  calcularTotal() {
+    this.resultado = this.qtdHotdog * 5;
+    this.resultado += this.qtdRefri * 3.5;
+    this.resultado += this.qtdBala * 0.25;
+    this.resultado += this.qtdChocolate * 2;
+
+    this.calcularTroco();
   }
+
+  calcularTroco() {
+    if (this.valorRecebido > 0)
+      this.resultadoTroco = this.valorRecebido - this.resultado;
+  }
+
   addHotdog() {
     this.qtdHotdog++;
     this.calcularTotal();
   }
-  delHotdog(){
-    if(this.qtdHotdog > 0)
-    this.qtdHotdog--;
+  delHotdog() {
+    if (this.qtdHotdog > 0) this.qtdHotdog--;
     this.calcularTotal();
   }
 
-  addRefri(){
+  addRefri() {
     this.qtdRefri++;
     this.calcularTotal();
-
   }
-  delRefri(){
-    if(this.qtdRefri >0)
-    this.qtdRefri--
+  delRefri() {
+    if (this.qtdRefri > 0) this.qtdRefri--;
     this.calcularTotal();
-
   }
 
-  addBala(){
+  addBala() {
     this.qtdBala++;
     this.calcularTotal();
-    
   }
-  delBala(){
-    if(this.qtdBala > 0)
-    this.qtdBala--;
+  delBala() {
+    if (this.qtdBala > 0) this.qtdBala--;
     this.calcularTotal();
-
   }
-  addChocolate(){
+  addChocolate() {
     this.qtdChocolate++;
     this.calcularTotal();
-    
   }
-  delChocolate(){
-    if(this.qtdChocolate > 0)
-    this.qtdChocolate--;
-    
+  delChocolate() {
+    if (this.qtdChocolate > 0) this.qtdChocolate--;
   }
-  limpar(){
-    this.qtdBala=0;
-    this.qtdChocolate=0;
-    this.qtdHotdog=0;
-    this.qtdRefri=0;
-    this.resultado=0;
+  limpar() {
+    this.qtdBala = null;
+    this.qtdChocolate = null;
+    this.qtdHotdog = null;
+    this.qtdRefri = null;
+    this.resultado = null;
+    this.resultadoTroco = null;
+    this.valorRecebido = null;
   }
-  
-  
 }
